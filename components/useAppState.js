4876641df1b72d7b91da51d9c9aa9466d596d487
Taegus <taegus.cromis@gmail.com>
2019-07-12 16:30:46 +0200

    moved toast dialog functions to utils.js mask address is made safer

--- components/useAppState.js
+++ components/useAppState.js
@@ -1,9 +1,11 @@
 import { useReducer, useRef } from 'react';
 import mergeJSON from 'merge-json';
 import { appSettings } from '../constants/appSettings';
-import { AppColors } from '../constants/Colors';
 import { logger } from '../helpers/Logger';
-import Toast from 'react-native-root-toast';
+import {
+  showErrorToast,
+  showSuccessToast
+} from '../helpers/utils';
 
 const useAppState = () => {
   const initialState = {
@@ -254,16 +256,7 @@ const useAppState = () => {
       case 'DISPLAY_MESSAGE':
         if (!action.message) action.message = {};
         if (action.message.join() !== '') {
-          let toast = Toast.show(action.message.join(), {
-            backgroundColor: AppColors.concealErrorColor,
-            duration: Toast.durations.LONG,
-            opacity: 1,
-            position: 0,
-            animation: true,
-            hideOnPress: true,
-            shadow: true,
-            delay: 300
-          });
+          showErrorToast(action.message.join());
         }
         result = {
           ...state,
@@ -283,16 +276,7 @@ const useAppState = () => {
         };
         break;
       case 'PAYMENT_SENT':
-        let toast = Toast.show('Payment was succesfully sent to the recipient', {
-          backgroundColor: AppColors.concealInfoColor,
-          duration: Toast.durations.LONG,
-          opacity: 1,
-          position: 0,
-          animation: true,
-          hideOnPress: true,
-          shadow: true,
-          delay: 300
-        });
+        showSuccessToast(action.message.join());
         result = {
           ...state
         };
--- helpers/utils.js
+++ helpers/utils.js
@@ -1,9 +1,41 @@
 import { appSettings } from '../constants/appSettings';
+import { AppColors } from '../constants/Colors';
+import Toast from 'react-native-root-toast';
 
 export const maskAddress = (address, maskingChar = '.', maskedChars = 8, charsPre = 7, charsPost = 7) => {
-  const pre = address.substring(0, charsPre);
-  const post = address.substring(address.length - charsPost);
-  return `${pre}${maskingChar.repeat(maskedChars)}${post}`;
+  if (address) {
+    const pre = address.substring(0, charsPre);
+    const post = address.substring(address.length - charsPost);
+    return `${pre}${maskingChar.repeat(maskedChars)}${post}`;
+  } else {
+    return '';
+  }
+};
+
+export const showErrorToast = (message) => {
+  let toast = Toast.show(message, {
+    backgroundColor: AppColors.concealErrorColor,
+    duration: Toast.durations.LONG,
+    opacity: 1,
+    position: 0,
+    animation: true,
+    hideOnPress: true,
+    shadow: true,
+    delay: 300
+  });
+};
+
+export const showSuccessToast = (message) => {
+  let toast = Toast.show(message, {
+    backgroundColor: AppColors.concealInfoColor,
+    duration: Toast.durations.LONG,
+    opacity: 1,
+    position: 0,
+    animation: true,
+    hideOnPress: true,
+    shadow: true,
+    delay: 300
+  });
 };
 
 export const format0Decimals = { minimumFractionDigits: 0, maximumFractionDigits: 0 };
