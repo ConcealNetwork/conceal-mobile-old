1712e5d0e4a0b4d6c512c150aba514b5fe7f7302
Taegus <taegus.cromis@gmail.com>
2019-07-21 18:23:32 +0200

    Revert "removed bottom border for edit address buttons"
    This reverts commit 0fe22ac346a6e528d459c1059f53baf074046f76.

--- screens/EditAddress.js
+++ screens/EditAddress.js
@@ -195,6 +195,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
     borderWidth: 0,
     borderRadius: 0,
+    borderBottomWidth: 2,
     borderColor: '#FFA500',
     backgroundColor: 'rgba(0, 0, 0, 0)'
   },
